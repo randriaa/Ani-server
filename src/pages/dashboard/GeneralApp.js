755b1b70f6149c72510de30b5883a3965a0f2889
randriaa <randrianarizanany@gmail.com>
2023-12-05 01:21:26 +0100

    [FEATURE] Ajout de la conversation

--- src/pages/dashboard/GeneralApp.js
+++ src/pages/dashboard/GeneralApp.js
@@ -1,14 +1,30 @@
 import React from "react";
 import Chats from "./Chats";
-import { Stack } from "@mui/material";
+import { Box, Stack } from "@mui/material";
+import Conversation from "../../components/Conversation";
+import { useTheme } from "@mui/material/styles";
 
 const GeneralApp = () => {
+
+  const theme = useTheme();
 
   return (
     <>
       <Stack>
         {/* Chats */}
-        <Chats></Chats>
+        <Chats />
+
+      </Stack>
+      <Stack>
+        <Box sx={{
+          height: "100%",
+          width: "calc(100vw - 420px)",
+          backgroundColor: theme.palette.mode === "light" ? "#fff" : "#000",
+        }}>
+          {/* Conversation */}
+          <Conversation></Conversation>
+
+        </Box>
       </Stack>
 
     </>
